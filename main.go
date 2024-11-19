2f08c8f0b1a9021827f821f9ac52ac170426914f
Spandan Majumder <spandan301298@gmail.com>
2024-11-20 00:08:44 +0530

    added a POST req

--- main.go
+++ main.go
@@ -20,7 +20,44 @@ func GetEvents(c *gin.Context) {
 	c.JSON(http.StatusOK, events)
 }
 
+// func createEvent(c *gin.Context) {
+// 	var event models.Event
+// 	err := c.BindJSON(&event)
+
+// 	if err != nil {
+// 		c.JSON(http.StatusBadRequest, gin.H{
+// 			"error": "Invalid request",
+// 		})
+// 		return
+// 	}
+
+// 	event.ID = 1
+// 	event.UserID = 1
+// 	c.JSON(http.StatusCreated, gin.H{"message": "event created successfully", "event": event})
+
+// }
+
 func createEvent(c *gin.Context) {
 	var event models.Event
-	c.BindJSON(&event)
+
+	// Bind JSON and validate
+	if err := c.ShouldBindJSON(&event); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error":   "Invalid request",
+			"details": err.Error(),
+		})
+		return
+	}
+
+	// Set ID and UserID manually (or generate dynamically)
+	event.ID = len(models.GetEvents()) + 1
+	event.UserID = 1
+
+	// Save the event
+	event.Save()
+
+	c.JSON(http.StatusCreated, gin.H{
+		"message": "Event created successfully",
+		"event":   event,
+	})
 }
